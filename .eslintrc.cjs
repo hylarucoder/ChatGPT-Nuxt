ac7b270ccb9b7659d1c3e0f046bdac436dae8f90
twocucao <twocucao@gmail.com>
2023-06-28 01:27:53 +0800

    add lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,16 @@
-require("@rushstack/eslint-patch/modern-module-resolution")
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-    parser: 'vue-eslint-parser',
-    "parserOptions": {
-        "parser": "@typescript-eslint/parser",
-        "sourceType": "module"
-    },
-    extends: [
-        // ... other configs
-        "plugin:vue/base",
-        "eslint:recommended",
-        "@vue/eslint-config-prettier",
-    ],
-}
+  parser: "vue-eslint-parser",
+  "parserOptions": {
+    "parser": "@typescript-eslint/parser",
+    "sourceType": "module"
+  },
+  extends: [
+    // ... other configs
+    "plugin:vue/base",
+    "eslint:recommended",
+    "plugin:nuxt/recommended",
+    "@vue/eslint-config-prettier"
+  ]
+};
