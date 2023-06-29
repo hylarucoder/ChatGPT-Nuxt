e7020200a9d28a87a6e95cb17838797ab752d7e6
twocucao <twocucao@gmail.com>
2023-06-29 16:30:11 +0800

    remove home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,4 @@
 <script setup lang="ts">
-const online = useOnline()
+const router = useRouter()
+router.push("/chat")
 </script>
-
-<template>
-  <div>
-    <Logos mb-6 />
-    <Suspense>
-      <ClientOnly>
-        <PageView v-if="online" />
-        <div v-else>lllllll You're offline</div>
-      </ClientOnly>
-      <template #fallback>
-        <div class="italic op50">
-          <span animate-pulse>Loading...</span>
-        </div>
-      </template>
-    </Suspense>
-    <InputEntry />
-  </div>
-</template>
