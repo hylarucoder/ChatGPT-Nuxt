e84f8cd826fcca4f8eb7850f2dfadf2579e35737
twocucao <twocucao@gmail.com>
2023-07-01 12:39:51 +0800

    format class

--- pages/chat/plugins.vue
+++ pages/chat/plugins.vue
@@ -1,3 +1,5 @@
 <template>
-  <div class="items-center text-zinc-800 flex">plugins</div>
+  <div class="items-center text-zinc-800 flex">
+    <div class="text-gray-700 shadow-md p-3 border-gray-300 ml-4 h-24 flex border-2">plugins</div>
+  </div>
 </template>
