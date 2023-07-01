589a5483fc08b2dc07c5623e0daa65771763410f
twocucao <twocucao@gmail.com>
2023-07-01 12:41:44 +0800

    auto order

--- pages/chat/plugins.vue
+++ pages/chat/plugins.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="items-center text-zinc-800 flex">
-    <div class="text-gray-700 shadow-md p-3 border-gray-300 ml-4 h-24 flex border-2">plugins</div>
+  <div class="flex items-center text-zinc-800">
+    <div class="ml-4 flex h-24 border-2 border-gray-300 p-3 text-gray-700 shadow-md">plugins</div>
   </div>
 </template>
