8bdd7ad666731ca56d861419e757fc49a14793d4
twocucao <twocucao@gmail.com>
2023-07-02 16:44:54 +0800

    remove some icon

--- pages/chat/new.vue
+++ pages/chat/new.vue
@@ -90,16 +90,21 @@ onMounted(async () => {
 })
 </script>
 <template>
-  <div class="flex w-full flex-shrink flex-col items-center">
-    <div class="flex w-full justify-between p-3">
+  <div class="flex w-full flex-shrink flex-col items-center overflow-hidden">
+    <div class="flex w-full justify-between p-3 text-zinc-800">
       <button
+        class="flex h-10 cursor-pointer items-center justify-center overflow-hidden rounded-xl bg-white p-3 text-center hover:bg-gray-200"
         @click="router.back()"
-        class="flex h-10 cursor-pointer items-center justify-center truncate rounded-xl bg-white p-3 text-center"
       >
         <div class="flex items-center justify-center">
           <VSvgIcon icon="return" class="h-4 w-4" />
         </div>
-        <div class="ml-1 truncate text-[0.75rem]">返回</div>
+        <div class="ml-1 overflow-hidden text-ellipsis text-xs">Return</div>
+      </button>
+      <button
+        class="flex h-10 cursor-pointer items-center justify-center overflow-hidden rounded-xl bg-white p-3 text-center hover:bg-gray-200"
+      >
+        <div class="ml-1 overflow-hidden text-ellipsis text-xs">Never Show Again</div>
       </button>
     </div>
     <div class="mb-5 mt-12 flex space-x-1">
@@ -117,22 +122,22 @@ onMounted(async () => {
     <div>现在开始，与面具背后的灵魂思维碰撞</div>
     <div class="mb-5 mt-12 flex justify-center space-x-2 text-[0.75rem]">
       <button
-        class="ml-3 flex h-10 w-24 cursor-pointer items-center justify-center truncate rounded-xl bg-emerald-400 p-3 text-center text-white hover:bg-emerald-500"
-        @click="newSessionAndNav"
-      >
-        <div class="flex items-center justify-center">
-          <VSvgIcon icon="lightning" class="" style="color: white" />
-        </div>
-        <div class="ml-1 truncate text-white">直接开始</div>
-      </button>
-      <button
         class="flex h-10 w-24 cursor-pointer items-center justify-center truncate rounded-xl border border-neutral-200 bg-white p-3 text-center hover:bg-gray-200"
         @click="router.push('/chat/masks')"
       >
         <div class="flex items-center justify-center">
-          <VSvgIcon icon="eye" class="h-4 w-4" />
+          <Icon size="1.3em" name="ph:eye" color="#000" />
         </div>
         <div class="ml-1 truncate text-black">查看全部</div>
+      </button>
+      <button
+        class="ml-3 flex h-10 w-24 cursor-pointer items-center justify-center truncate rounded-xl bg-[--primary] p-3 text-center text-white hover:bg-emerald-500"
+        @click="newSessionAndNav"
+      >
+        <div class="flex items-center justify-center">
+          <Icon color="#FFF" size="1.4em" name="typcn:flash-outline" />
+        </div>
+        <div class="ml-1 truncate text-white">直接开始</div>
       </button>
     </div>
     <div class="flex-grow items-center overflow-x-hidden pt-5">
