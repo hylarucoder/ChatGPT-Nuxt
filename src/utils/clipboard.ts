ea1b63f00546c7a432f91432b3b767dc0d3ccada
twocucao <twocucao@gmail.com>
2023-07-05 16:40:47 +0800

    clipboard v1

--- src/utils/clipboard.ts
+++ src/utils/clipboard.ts
@@ -1,11 +1,11 @@
 export const copyToClipboard = (content: string) => {
-  // copy
-  const input = document.createElement("input")
+  const input = document.createElement("textarea")
+  input.innerHTML = content
   input.setAttribute("readonly", "readonly")
   input.setAttribute("value", content)
   document.body.appendChild(input)
   input.select()
-  input.setSelectionRange(0, 9999)
+  input.setSelectionRange(0, 99999)
   document.execCommand("copy")
   document.body.removeChild(input)
 }
