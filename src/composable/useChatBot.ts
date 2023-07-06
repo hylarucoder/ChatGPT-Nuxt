22c66584d5513cf596bde38aab253a57d6b0ad9e
rogepi <zzr1119@outlook.com>
2023-07-06 16:06:47 +0800

    fix useChatBot

--- src/composable/useChatBot.ts
+++ src/composable/useChatBot.ts
@@ -31,6 +31,7 @@ export default function useChatBot() {
       headers: {
         Accept: "text/event-stream",
         Authorization: `Bearer ${TOKEN}`,
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         ...payload,
@@ -64,7 +65,7 @@ export default function useChatBot() {
             onMessage(message)
           }
         } catch (e) {
-          message.content = String(s)
+          message.content = String(e)
           onError(message)
         }
       },
