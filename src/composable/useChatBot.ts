c733bba328394dbc058f32bc16f98b4f6b99ec98
HylaruCoder <twocucao@gmail.com>
2023-07-07 14:29:29 +0800

    stop retry (#55)
    * stop retry
    
    * stop retry

--- src/composable/useChatBot.ts
+++ src/composable/useChatBot.ts
@@ -26,57 +26,74 @@ export default function useChatBot() {
     const TOKEN = settings.apiKey
     const API_URL = `${BASE_URL}/v1/chat/completions`
 
-    await fetchEventSource(API_URL, {
-      method: "POST",
-      headers: {
-        Accept: "text/event-stream",
-        Authorization: `Bearer ${TOKEN}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        ...payload,
-      }),
-      // eslint-disable-next-line require-await
-      async onopen(res) {
-        if (res.ok && res.status === 200) {
-          console.log("Connection made ", res)
-          message.status = "connected"
-          onStart(message)
-        } else if (res.status >= 400 && res.status < 500 && res.status !== 429) {
-          message.status = "error"
-          message.errorMessage = `server side error ${res.status || res.statusText} ${res.type}`
-          onError(message)
-          console.log("Client-side error ", res)
-        }
-      },
-      onmessage(ev) {
-        if (ev.data === "[DONE]") {
-          message.status = "done"
-          onDone(message)
-          return
-        }
-        if (!ev.data) {
-          return
-        }
-        try {
-          const parsedData = JSON.parse(ev.data)
-          const content = parsedData.choices[0]?.delta?.content
-          if (content) {
-            message.content += content
-            onMessage(message)
+    const controller = new AbortController()
+
+    try {
+      await fetchEventSource(API_URL, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...payload,
+        }),
+        signal: controller.signal,
+
+        async onopen(res) {
+          if (res.ok && res.status === 200) {
+            console.log("Connection made ", res)
+            message.status = "connected"
+            onStart(message)
+            try {
+              const json = await res.json()
+              message.errorMessage = JSON.stringify(json.error)
+              onError(message)
+              controller.abort("Server Error")
+            } catch (e) {
+              controller.abort("Error parsing response")
+            }
+          } else if (res.status >= 400 && res.status < 500 && res.status !== 429) {
+            message.status = "error"
+            message.errorMessage = `server side error ${res.status || res.statusText} ${res.type}`
+            onError(message)
+            controller.abort("Error parsing response")
+            console.log("Client-side error ", res)
           }
-        } catch (e) {
-          message.content = String(e)
+        },
+        onmessage(ev) {
+          console.log("messaging-side error ", ev.data)
+          if (ev.data === "[DONE]") {
+            message.status = "done"
+            onDone(message)
+            return
+          }
+          if (!ev.data) {
+            return
+          }
+          try {
+            const parsedData = JSON.parse(ev.data)
+            const content = parsedData.choices[0]?.delta?.content
+            if (content) {
+              message.content += content
+              onMessage(message)
+            }
+          } catch (e) {
+            message.content = String(e)
+            onError(message)
+            controller.abort("Error message response")
+          }
+        },
+        onerror(err) {
+          message.status = "error"
+          message.errorMessage = `error ${err.status || err.statusText || ""} ${err.type || ""}`
           onError(message)
-        }
-      },
-      onerror(err) {
-        message.status = "error"
-        message.errorMessage = `error ${err.status || err.statusText || ""} ${err.type || ""}`
-        onError(message)
-        throw err
-      },
-    })
+          throw err
+        },
+      })
+    } catch (e) {
+      message.content = String(e)
+    }
   }
 
   return {
